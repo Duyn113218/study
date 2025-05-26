06fd9962e7507968adaa6ab311441b48b2e022e9
Duyn <duyen91thcstp@ggmail.com>
2025-05-26 22:52:47 +0700

    AAA

--- Example2.java
+++ Example2.java
@@ -16,3 +16,23 @@ public class RecursiveFibonacci {
         }
     }
 }
+
+Đây là nhánh 2 Ạ
+public class RecursiveFibonacci {
+
+    // Hàm tính Fibonacci bằng đệ quy
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.print("Dãy Fibonacci (đệ quy): ")
+        for (int i = 0; i < count; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
